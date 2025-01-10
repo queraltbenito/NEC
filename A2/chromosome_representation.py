3ac4c4b6d20d353cf6a24f2147ab1f3cc1f81e58
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-10 19:18:45 +0100

    initial population

--- A2/chromosome_representation.py
+++ A2/chromosome_representation.py
@@ -1,1 +1,19 @@
+import random 
 
+def generate_chromosome_items(total_tasks):
+    items = []
+    for (job_id, job) in enumerate(total_tasks):
+        for (task_id, task) in enumerate(job):
+            items.append(f"{job_id}-{task_id}")
+    return items
+
+def valid_chromosome(chromosome):
+    return True
+
+def generate_chromosome(items):
+    # A chromosome is a random order of the list of items
+    chromosome = items.copy()
+    random.shuffle(chromosome)
+    while not valid_chromosome(chromosome):
+        random.shuffle(chromosome)
+    return chromosome
