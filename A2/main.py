39627e3700dc29bf2ed77f23e4340d7f3cc6cfa9
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-11 12:47:51 +0100

    GA algorithm

--- A2/main.py
+++ A2/main.py
@@ -1,5 +1,9 @@
 from chromosome_representation import generate_chromosome_items, generate_chromosome
 from fitness import compute_duration
+from selection import roulette_wheel_selection, stochastic_universal_sampling
+from crossover import one_point_crossover, two_point_crossover
+from mutation import one_mutation, prob_mutation
+from elitism import elitism
 
 def read_data(path):
     with open(path, 'r') as file:
@@ -16,6 +20,7 @@ def read_data(path):
     return j, m, tasks
 
 if __name__ == "__main__":
+    # Input data
     path = "./dataset1.txt"
     num_jobs, num_machines, tasks = read_data(path)
     
@@ -23,10 +28,47 @@ if __name__ == "__main__":
     print(num_machines)
     print(tasks)
 
+    # Initialize population P
+    # Evaluate fitness of all individuals in P
     items = generate_chromosome_items(tasks)
-    population_size = 1
-    population = [generate_chromosome(items, num_jobs) for _ in range(population_size)]
-    print(population)
+    population_size = 5
+    population = []
+    for _ in range(population_size):
+        chromosome = generate_chromosome(items, num_jobs)
+        duration = compute_duration(chromosome, num_jobs, num_machines, tasks)
+        population.append((chromosome, 1/duration))
 
-    duration = compute_duration(population[0], num_jobs, num_machines, tasks)
-    print(duration)
+    # Genetic Algorithm
+    num_generation = 2
+    for generation in range(num_generation):
+        population_size = len(population)
+        new_population = []
+        for pair in range(int(population_size/2)):
+            # 1) Define new chromosomes
+            # Selection process
+            parent1 = roulette_wheel_selection(population)
+            parent2 = roulette_wheel_selection(population)
+            # Crossover process
+            child1, child2 = one_point_crossover(parent1, parent2) 
+            # Mutation process
+            mutated_child1 = one_mutation(child1)
+            mutated_child2 = one_mutation(child2)
+
+            # 2) Compute new fitness
+            duration1 = compute_duration(mutated_child1, num_jobs, num_machines, tasks)
+            duration2 = compute_duration(mutated_child2, num_jobs, num_machines, tasks)
+
+            # 3) Add new element to the population
+            new_population.append((mutated_child1, 1/duration1))
+            new_population.append((mutated_child2, 1/duration2))
+
+        
+        # Elitism. Add best fitted individuals of P to P'
+        new_population.extend(elitism(population, 5))
+
+        population = new_population
+    
+    best_item = elitism(population, 1)[0]
+    best_chromosome = best_item[0]
+    best_duration = 1/best_item[1]
+    print(best_chromosome, best_duration)
