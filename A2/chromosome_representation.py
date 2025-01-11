64607a0397ecb8f8bb9ba09f7705716446e30c4f
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-11 11:39:56 +0100

    new representation

--- A2/chromosome_representation.py
+++ A2/chromosome_representation.py
@@ -3,17 +3,12 @@ import random
 def generate_chromosome_items(total_tasks):
     items = []
     for (job_id, job) in enumerate(total_tasks):
-        for (task_id, task) in enumerate(job):
-            items.append(f"{job_id}-{task_id}")
+        items.extend([job_id] * len(job))
     return items
 
-def valid_chromosome(chromosome):
-    return True
 
-def generate_chromosome(items):
+def generate_chromosome(items, num_jobs):
     # A chromosome is a random order of the list of items
     chromosome = items.copy()
     random.shuffle(chromosome)
-    while not valid_chromosome(chromosome):
-        random.shuffle(chromosome)
     return chromosome
