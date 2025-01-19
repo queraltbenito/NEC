4298674c44386d86a8316a9c42d642da66eb5754
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-19 17:51:16 +0100

    one_mutation

--- A2/mutation.py
+++ A2/mutation.py
@@ -1,1 +1,11 @@
+import random 
 
+def one_mutation(chromosome):
+    if len(chromosome) < 2: return chromosome
+    
+    id1, id2 = random.sample(range(len(chromosome)), 2)
+    chromosome[id1], chromosome[id2] = chromosome[id2], chromosome[id1]
+    return chromosome
+
+def prob_mutation():
+    return
