35317fceaa070fbfa8fd7f176b96933fadb184e8
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-19 20:43:14 +0100

    add counter

--- A2/chromosome_representation.py
+++ A2/chromosome_representation.py
@@ -2,9 +2,11 @@ import random
 
 def generate_chromosome_items(total_tasks):
     items = []
+    counter_tasks_job = []
     for (job_id, job) in enumerate(total_tasks):
+        counter_tasks_job.append(len(job))
         items.extend([job_id] * len(job))
-    return items
+    return counter_tasks_job, items
 
 
 def generate_chromosome(items, num_jobs):
