230921c468e13e5c6e554136f60aca258632c1ee
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-19 17:54:19 +0100

    crossover (falta)

--- A2/crossover.py
+++ A2/crossover.py
@@ -1,1 +1,16 @@
+import random
+from collections import Counter
 
+def one_point_crossover(parent1, parent2):
+    crossover_point = random.randint(1, len(parent1) - 1)
+
+    child1 = parent1[:crossover_point]
+    child2 = parent2[:crossover_point]
+    
+    #### FALTA
+    return child1, child2
+
+def two_point_crossover(parent1, parent2):
+    child1 = parent1
+    child2 = parent2
+    return child1, child2
