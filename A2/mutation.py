a93c9288b010ab50c1b1561992a8c9586cb71190
queraltbenito <89044204+queraltbenito@users.noreply.github.com>
2025-01-20 23:48:37 +0100

    prob mutation

--- A2/mutation.py
+++ A2/mutation.py
@@ -7,5 +7,11 @@ def one_mutation(chromosome):
     chromosome[id1], chromosome[id2] = chromosome[id2], chromosome[id1]
     return chromosome
 
-def prob_mutation():
-    return
+def prob_mutation(chromosome, mutation_prob=0.1):
+    for i in range(len(chromosome)):
+        if random.random() < mutation_prob:
+            # Select a random index to swap with
+            swap_idx = random.randint(0, len(chromosome) - 1)
+            # Swap the two genes
+            chromosome[i], chromosome[swap_idx] = chromosome[swap_idx], chromosome[i]
+    return chromosome
